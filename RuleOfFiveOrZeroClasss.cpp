cf0d83ae7aedef7c1c80ec40b5db3605ba90ff01
Maciej Białczak <m.bialczak@protonmail.com>
2022-11-06 22:18:59 +0100

    Rule of 0 implemented - using shared_ptr

--- RuleOfFiveOrZeroClasss.cpp
+++ RuleOfFiveOrZeroClasss.cpp
@@ -4,45 +4,13 @@ template <typename T>
 class Rules
 {
   public:
-    Rules()
-        : resource_(nullptr) { }
-    ~Rules()
-    {
-        delete resource_;
-    }
-
     Rules(T* resourcePtr)
         : resource_(resourcePtr)
     {
     }
 
-    Rules(const Rules& other)
-        : resource_(new T { *other.resource_ }) { }
-
-    Rules& operator=(const Rules& other)
-    {
-        delete resource_;
-        resource_ = new T { *other.resource_ };
-
-        return *this;
-    }
-
-    Rules(Rules&& other)
-        : resource_(other.resource_)
-    {
-        other.resource_ = nullptr;
-    }
-    Rules& operator=(Rules&& other)
-    {
-        delete resource_;
-        resource_ = other.resource_;
-        other.resource_ = nullptr;
-
-        return *this;
-    }
-
   private:
-    T* resource_;
+    std::shared_ptr<T> resource_;
 };
 
 int main(int argc, const char** argv)
