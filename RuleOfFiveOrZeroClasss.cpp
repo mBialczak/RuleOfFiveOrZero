b41eccfbca02aeab14e9a6b07bf91054fc24dccd
Maciej Białczak <m.bialczak@protonmail.com>
2022-11-06 22:21:31 +0100

    Rule of 0 implemented - using unique_ptr;

--- RuleOfFiveOrZeroClasss.cpp
+++ RuleOfFiveOrZeroClasss.cpp
@@ -10,14 +10,14 @@ class Rules
     }
 
   private:
-    std::shared_ptr<T> resource_;
+    std::unique_ptr<T> resource_;
 };
 
 int main(int argc, const char** argv)
 {
     Rules<int> r(new int { 42 });
-    auto r2 = r;
-    r = r2;
+    // auto r2 = r; // copying deleted
+    // r = r2;
     auto r3 = std::move(r);
     r = std::move(r3);
 
